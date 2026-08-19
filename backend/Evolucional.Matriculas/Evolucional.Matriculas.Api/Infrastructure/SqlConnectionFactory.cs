bb58332d427746578af94e6add44f5efe84450e4
Ricardo Vieira <ricardovieirasp@gmail.com>
2026-08-19 10:08:40 -0300

    feat: add application structure and SQL connection factory

---
+++ backend/Evolucional.Matriculas/Evolucional.Matriculas.Api/Infrastructure/SqlConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Evolucional.Matriculas.Api.Infrastructure
+{
+    public class SqlConnectionFactory
+    {
+        private readonly string _connectionString;
+        public SqlConnectionFactory()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["TesteEscola"].ConnectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
---
+++ backend/Evolucional.Matriculas/Evolucional.Matriculas.Api/Models/Aluno.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Evolucional.Matriculas.Api.Models
+{
+    public class Aluno
+    {
+    }
+}
